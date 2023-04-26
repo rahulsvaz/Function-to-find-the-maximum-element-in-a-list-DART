559672e1c5f5bb8202b606ff9c81df70d8abb660
Rahul P <128817814+rahulsvaz@users.noreply.github.com>
2023-04-27 02:18:53 +0530

    Add files via upload

---
+++ find maximum value.dart
@@ -0,0 +1,14 @@
+// Main function
+void main() {
+
+
+  
+  var numbers = [121, 12, 33, 14, 3];
+    
+  // Sorting the list
+  numbers.sort();
+  
+  // Printing the values
+  print("Smallest value in the list : ${numbers.first}");
+  print("Largest value in the list : ${numbers .last}");
+}
